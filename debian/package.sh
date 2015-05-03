a4f546a9643fb3d756d7f448fb5a77863cc9b3cc
Dominik Stadler <dominik.stadler@gmx.at>
2015-05-03 11:19:42 +0200

    Add script to create diff against current git

---
+++ debian/package.sh
@@ -0,0 +1,32 @@
+# to create diff
+#
+
+DATE=`date +%Y%m%d`
+
+# git fetch && git rebase origin/master && \
+#quilt pop -a ; \
+
+echo "Git Status" && \
+cd ../openambit.git && \
+git status && \
+\
+cd ../openambit && \
+echo "Status" && \
+git status && \
+echo "Create diff" && \
+diff -Nur -x debian -x .git -x .gitignore -x .pc . ../openambit.git > debian/patches/01_git${DATE}.patch ; \
+echo "Series" && \
+echo 01_git${DATE}.patch > debian/patches/series && \
+\
+\
+echo "All done"
+
+#echo "libambit" && \
+#cd ../libambit && \
+#git status ; \
+#echo "diff" && \
+#diff -Nur -x debian -x .git -x .pc . ../openambit.git/src/libambit/ > debian/patches/01_git${DATE}.patch ; \
+#echo "series" && \
+#echo 01_git${DATE}.patch > debian/patches/series && \
+#echo "#02_use_hid_package.patch" >> debian/patches/series && \
+#\
