da75426a37ea19c07d021df11bcf2e48f2336853
Dominik Stadler <dominik.stadler@gmx.at>
2015-06-14 09:50:27 +0200

    Lower cmake dependencies to 2.8.5, seems to still work and avoid having to backport newer cmake to trusty

--- debian/package.sh
+++ debian/package.sh
@@ -16,7 +16,7 @@ git status && \
 echo "Create diff" && \
 diff -Nur -x debian -x .git -x .gitignore -x .pc . ../openambit.git > debian/patches/01_git${DATE}.patch ; \
 echo "Series" && \
-echo -e "01_git${DATE}.patch\ninstall_example" > debian/patches/series && \
+echo -e "01_git${DATE}.patch\ninstall_example\nlower_cmake_dep" > debian/patches/series && \
 \
 \
 echo "All done"
